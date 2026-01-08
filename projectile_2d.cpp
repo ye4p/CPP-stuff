0ac82943871a1903f3f6ba7d069ad3aa308722c8
Danila Golubkov <danilgolubkov172@gmail.com>
2026-01-08 15:46:03 -0500

    fix

--- projectile_2d.cpp
+++ projectile_2d.cpp
@@ -5,15 +5,16 @@
 #include <windows.h>
 using namespace std;
 
-void gotoxy(int x, int y) {
+void gotoxy(int x, int y)
+{
     COORD coord = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
 int main()
 {
-    string output="";
-    gotoxy(0,0);
+    string output = "";
+    gotoxy(0, 0);
     double v, T, angleDegrees;
     // cout << "Initial height: ";
     // cin >> hi;
@@ -25,24 +26,51 @@ int main()
     T = 10;
     const double g = 9.8;
     const double dt = .05;
-    angleDegrees= 60;
-    double angleRadians=angleDegrees*numbers::pi/ 180.0;
+    angleDegrees = 60;
+    const double pi = acos(-1);
+    double angleRadians = angleDegrees * pi / 180.0;
 
-    const int screenHeight = 36;   // number of rows in the ASCII screen
+    const int screenHeight = 36; // number of rows in the ASCII screen
+    const int screenWidth = 80;
     const double maxHeight = 36.0; // map 20 meters to 20 rows
-    for (double t=0; t<=T; t+=dt) {
+    const double maxWidth = 80.0;
+    for (double t = 0; t <= T; t += dt)
+    {
         double vx = v * cos(angleRadians);
-        double vy = v * sin(angleRadians)- g*t;
-        double h = v * sin(angleRadians)*t - g*t*t/2;
-        double x = vx*t;
-        int ballrow;
-        if (h<=0 && t>0) {
+        double vy = v * sin(angleRadians) - g * t;
+        double h = v * sin(angleRadians) * t - g * t * t / 2;
+        double x = vx * t;
+        if (h <= 0 && t > 0)
+        {
             break;
         }
         int ballRow = screenHeight - (int)((h / maxHeight) * screenHeight);
-        
+        int ballCol = (int)((x / maxWidth) * screenWidth);
+        string output = "";
+        gotoxy(0, 0);
+        if (ballRow < 0)
+            ballRow = 0;
+        if (ballRow >= screenHeight)
+            ballRow = screenHeight - 1;
+        // if (ballCol < 0)
+        //     ballCol = 0;
+        if (ballCol >= screenWidth)
+            ballCol = screenWidth - 1;
+        for (int row = 0; row < screenHeight; row++)
+        {
+            for (int column = 0; column < screenWidth; column++)
+            {
+                if (row == ballRow && column == ballCol)
+                    output += "O";
+                else
+                    output += " ";
+            }
+            output += "\n";
+        }
+        // cout << "vx: " << vx << " vy: " << vy << " h: " << h << "x: " << x << "\n";
 
-        //cout << "vx: " << vx << " vy: " << vy << " h: " << h << "\n";
-
+        output += "                                     ";
+        cout << output << endl;
+        this_thread::sleep_for(chrono::milliseconds(50));
     }
-} 
+}
